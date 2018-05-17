3db084c288d008ddce735f34ee73e94c751fdc5e
Daniel Wright <rlinks234@gmail.com>
2018-05-17 11:25:58 -0400

    Remove unused function

--- kits/util/DemoUtils.py
+++ kits/util/DemoUtils.py
@@ -11,19 +11,3 @@ def retry_on_error(func, on_error_func=None, sleep_time=0.1):
       if on_error_func != None:
         on_error_func()
       sleep(sleep_time)
-
-
-def send_gains(group, gains_file):
-  command = GroupCommand(group.size)
-  command.read_gains(gains_file)
-
-  # Disable feedback frequency to reduce messages
-  prev_freq = group.feedback_frequency
-  group.feedback_frequency = 0
-
-  sleep(0.25)
-  for i in range(5):
-    group.send_command(command)
-    sleep(0.1)
-
-  group.feedback_frequency = prev_freq
